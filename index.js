59e6a58f831899847acc39ee89f72ff9e67b92fc
Josy Hartig <josy_hartig@yahoo.de>
2019-09-28 15:15:00 +0800

    bug fixes

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
     if (this.scrollY >= 510) {
       $('#navbar').addClass('scroll');
       $('header').addClass('scroll-header');
+      $('navbar-icon').addClass('scroll-icon');
       $('.logo').removeClass('logo-big').addClass('scroll-logo');
     } else {
       $('#navbar').removeClass('scroll');
       $('header').removeClass('scroll-header');
+      $('navbar-icon').removeClass('scroll-icon');
       $('.logo').addClass('logo-big').removeClass('scroll-logo');
       }
     });
@@ -20,47 +22,44 @@
         });
       });
 
-  $(document).ready(function() {
-          $('.navbar-icon').click(function() {
-          $('#navbar-terms').slideToggle('slow');
-          $('.nav').addClass('clicked');
-        });
-      });
-
 // Close Navbar on click on navlink
   $('.nav-link').click(function(){
-    if (window.matchMedia("(max-width: 1050px)").matches) {
+    if (window.matchMedia("(max-width: 900px)").matches) {
     $('#navbar').slideUp('slow');
   } else {
     $('#navbar').css('display', 'block');
   }
   });
 
+// Close Navbar on click outside
+  $(document).on('click touchstart', function(e) {
+    if (window.matchMedia("(max-width: 900px)").matches){
+      if (!$('#navbar').is(e.target) && !$('.navbar-toggle').is(e.target) && !$('.nav').is(e.target)){
+    		$('#navbar').slideUp('slow');
+      }
+    }
+  	});
+
 // FadeUp Option
-
-AOS.init({// Global settings:
-disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-initClassName: 'aos-init', // class applied after initialization
-animatedClassName: 'aos-animate', // class applied on animation
-useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-
-// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-offset: 100, // offset (in px) from the original trigger point
-delay: 30, // values from 0 to 3000, with step 50ms
-duration: 1000, // values from 0 to 3000, with step 50ms
-easing: 'ease-in-out', // default easing for AOS animations
-once: true, // whether animation should happen only once - while scrolling down
-mirror: false, // whether elements should animate out while scrolling past them
-anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+AOS.init({
+disable: false,
+startEvent: 'DOMContentLoaded',
+initClassName: 'aos-init',
+animatedClassName: 'aos-animate',
+useClassNames: false,
+disableMutationObserver: false,
+debounceDelay: 50,
+throttleDelay: 99,
+offset: 100,
+delay: 30,
+duration: 1000,
+easing: 'ease-in-out',
+once: true,
+mirror: false,
+anchorPlacement: 'top-bottom',
 });
 
 // Scrollspy
-
 $(document).ready(function(){
   $('.scrollspy').scrollSpy();
 });
