ce431355f51c21c03e54c1ed9c296e0438ba8254
Josy Hartig <josy_hartig@yahoo.de>
2019-10-05 13:31:37 +0800

    layout update additional pages

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
-
 // Make Navbar appear dark if scrolled
-let elementPosition = $('.chevron').offset().top;
+if ($('#hero').length) {
+  let elementPosition = $('.chevron').offset().top;
 
-if(window.matchMedia("(max-width: 600px)").matches) {
-  elementPosition = $('.intro').offset().top - ($(window).height() * 0.25);
-}
-
-$(window).scroll(() => {
-  const yScrollPos = window.pageYOffset;
-  if (yScrollPos > elementPosition) {
-    $('#navbar').addClass('scroll');
-    $('header').addClass('scroll-header');
-    $('navbar-icon').addClass('scroll-icon');
-    $('.logo').removeClass('logo-big').addClass('scroll-logo');
-  } else {
-    $('#navbar').removeClass('scroll');
-    $('header').removeClass('scroll-header');
-    $('navbar-icon').removeClass('scroll-icon');
-    $('.logo').addClass('logo-big').removeClass('scroll-logo');
+  if(window.matchMedia("(max-width: 600px)").matches) {
+    elementPosition = $('.intro').offset().top - ($(window).height() * 0.25);
   }
-});
+
+  $(window).scroll(() => {
+    const yScrollPos = window.pageYOffset;
+    if (yScrollPos > elementPosition) {
+      $('#navbar').addClass('scroll');
+      $('header').addClass('scroll-header');
+      $('navbar-icon').addClass('scroll-icon');
+      $('.logo').removeClass('logo-big').addClass('scroll-logo');
+    } else {
+      $('#navbar').removeClass('scroll');
+      $('header').removeClass('scroll-header');
+      $('navbar-icon').removeClass('scroll-icon');
+      $('.logo').addClass('logo-big').removeClass('scroll-logo');
+    }
+  });
+}
 
 // Reveal Navbar on click for mobile devices
 $(document).ready(() => {
@@ -43,6 +44,7 @@ $('.nav-link').click(() => {
       $('#navbar').slideUp('slow');
       $('.overlay').removeClass('visible');
       $('body').css('overflow-y', 'auto');
+      $('.navbar-toggle').removeClass('open');
     }, 500);
   } else {
     $('#navbar').css('display', 'block');
@@ -73,20 +75,22 @@ $(document).ready(() => {
 });
 
 // FadeUp Option
-AOS.init({
-  disable: false,
-  startEvent: 'DOMContentLoaded',
-  initClassName: 'aos-init',
-  animatedClassName: 'aos-animate',
-  useClassNames: false,
-  disableMutationObserver: false,
-  debounceDelay: 50,
-  throttleDelay: 99,
-  offset: 50,
-  delay: 30,
-  duration: 1000,
-  easing: 'ease-in-out',
-  once: true,
-  mirror: false,
-  anchorPlacement: 'top-bottom',
-});
+if ($('#hero').length) {
+  AOS.init({
+    disable: false,
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
+    offset: 50,
+    delay: 30,
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+  });
+}
